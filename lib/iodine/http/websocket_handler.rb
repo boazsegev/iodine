03b4a6c05987c13568f5ba95dff80b6ba07d354d
Boaz Segev <Boaz@2be.co.il>
2015-10-19 02:55:40 -0400

    namespacing

--- lib/iodine/http/websocket_handler.rb
+++ lib/iodine/http/websocket_handler.rb
@@ -10,9 +10,31 @@ require 'zlib'
 # require 'securerandom'
 
 module Iodine
-  class WSEchoDemo
-  	
-  end
+	class Http < Iodine::Protocol
+		class WebsocketEchoDemo
+			def initialize request, response
+				@request = request
+				@response = response
+				on_open
+			end
+			def on_open
+			end
+			def on_message data
+				@response << "You >> #{data}"
+			end
+			def on_close
+			end
+
+			# This method allows the class itself to act as the Websocket handler, usable with:
+			#        Iodine::Http.on_websocket Iodine::Http::WebsocketEchoDemo
+			def self.call request, response
+				return false if request[:path] =~ /refuse/i
+				self.new request, response
+			end
+			
+			protected
+		end
+	end
 end
 
 
