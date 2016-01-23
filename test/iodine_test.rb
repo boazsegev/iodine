0cb2e607e591b990ef3566a585c0998f03446d1f
Boaz Segev <Boaz@2be.co.il>
2016-01-22 22:44:42 -0500

    brrr

--- test/iodine_test.rb
+++ test/iodine_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-Iodine.warn "Iodine only tests the HPACK encode/decode automatically. Most other tests are performed manualy at this time."
+warn "Iodine only tests the HPACK encode/decode automatically. Most other tests are performed manualy at this time."
 
 class IodineTest < Minitest::Test
   def test_that_it_has_a_version_number
