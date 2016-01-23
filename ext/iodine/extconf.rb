67739f9e9880a2b7a5135252bd42a48d56abb517
Boaz Segev <Boaz@2be.co.il>
2016-01-22 22:10:16 -0500

    c11?

--- ext/iodine/extconf.rb
+++ ext/iodine/extconf.rb
@@ -2,6 +2,6 @@ require "mkmf"
 
 abort "Missing a Linux/Unix OS evented API (epoll/kqueue)." unless have_func("kevent") || have_func("epoll_ctl")
 
-$CFLAGS = '-std=c11 -O3 -Wno-error=shorten-64-to-32 -Wall'
+$CFLAGS = '-std=c11 -O3 -Wall -g'
 
 create_makefile "iodine/iodine"
