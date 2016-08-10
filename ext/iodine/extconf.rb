0042529fb2a9a34fe162c4c6dc9dadca8c76485b
Boaz Segev <Boaz@2be.co.il>
2016-08-10 18:31:35 -0400

    minor updates

--- ext/iodine/extconf.rb
+++ ext/iodine/extconf.rb
@@ -8,22 +8,28 @@ if ENV['CC']
 elsif find_executable('clang')
   $CC = ENV['CC'] = 'clang'
   $CPP = ENV['CPP'] = 'clang'
-  puts 'detected clang compiler.'
+  puts 'using clang compiler.'
+elsif find_executable('gcc-6')
+  $CC = ENV['CC'] = 'gcc-6'
+  $CPP = ENV['CPP'] = find_executable('g++-6') ? 'g++-6' : 'gcc-6'
+  puts 'using gcc-6 compiler.'
 elsif find_executable('gcc-5')
   $CC = ENV['CC'] = 'gcc-5'
-  $CPP = ENV['CPP'] = 'gcc-5'
-  puts 'detected gcc-5 compiler.'
+  $CPP = ENV['CPP'] = find_executable('g++-5') ? 'g++-5' : 'gcc-5'
+  puts 'using gcc-5 compiler.'
 elsif find_executable('gcc-4.9')
   $CC = ENV['CC'] = 'gcc-4.9'
-  $CPP = ENV['CPP'] = 'gcc-4.9'
-  puts 'detected gcc-4.9 compiler.'
+  $CPP = ENV['CPP'] = find_executable('g++-4.9') ? 'g++-4.9' : 'gcc-4.9'
+  puts 'using gcc-4.9 compiler.'
+else
+  warn 'unknown / old compiler version - installation might fail.'
 end
 
 RbConfig::MAKEFILE_CONFIG['CC'] = $CC = ENV['CC'] if ENV['CC']
 RbConfig::MAKEFILE_CONFIG['CPP'] = $CPP = ENV['CPP'] if ENV['CPP']
 
 abort 'Missing a Linux/Unix OS evented API (epoll/kqueue).' unless have_func('kevent') || have_func('epoll_ctl')
-# abort 'Missing support for atomic operations (support for C11) - is your compiler updated?' unless have_header('stdatomic.h')
+abort 'Missing support for atomic operations (support for C11) - is your compiler updated?' unless have_header('stdatomic.h')
 # abort "Missing OpenSSL." unless have_library("ssl")
 
 create_makefile 'iodine/iodine'
