10a66611dc6796b7c810dc52ef24d81e550a5ac2
Boaz Segev <Boaz@2be.co.il>
2016-08-10 11:39:28 -0400

    less versions

--- ext/iodine/extconf.rb
+++ ext/iodine/extconf.rb
@@ -7,12 +7,12 @@ $CFLAGS = '-std=c11 -O3 -Wall'
 
 if find_executable('gcc')
   puts 'Attempting to set compiler to gcc.'
-  $CC = ENV['CC'] = 'gcc'
+  $CC = ENV['CC'] ||= 'gcc'
 elsif find_executable('clang')
   puts 'Attempting to set compiler to clang.'
-  $CC = ENV['CC'] = 'clang'
+  $CC = ENV['CC'] ||= 'clang'
 end
 
-# RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
+RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
 create_makefile 'iodine/iodine'
