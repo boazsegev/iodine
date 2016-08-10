f88c19f726d8ab15ea9cd33a30bf322a700bb2be
Boaz Segev <Boaz@2be.co.il>
2016-08-10 17:25:39 -0400

    required include

--- ext/iodine/extconf.rb
+++ ext/iodine/extconf.rb
@@ -1,16 +1,15 @@
 require 'mkmf'
 
 abort 'Missing a Linux/Unix OS evented API (epoll/kqueue).' unless have_func('kevent') || have_func('epoll_ctl')
+abort 'Missing support for atomic operations (support for C11) - is your compiler updated?' unless have_header('stdatomic.h')
 # abort "Missing OpenSSL." unless have_library("ssl")
 
 $CFLAGS = '-std=c11 -O3 -Wall'
 
 if find_executable('gcc')
-  puts 'Attempting to set compiler to gcc.'
   $CC = ENV['CC'] ||= 'gcc'
   # $CPP = ENV['CPP'] ||= 'gcc'
 elsif find_executable('clang')
-  puts 'Attempting to set compiler to clang.'
   $CC = ENV['CC'] ||= 'clang'
   # $CPP = ENV['CPP'] ||= 'clang'
 end
