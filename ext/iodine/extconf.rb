2c0dd42f5a12b852a1955651fdc6e47bd4609bb1
Boaz Segev <Boaz@2be.co.il>
2016-08-10 17:57:00 -0400

    first set the compiler - then check

--- ext/iodine/extconf.rb
+++ ext/iodine/extconf.rb
@@ -1,9 +1,5 @@
 require 'mkmf'
 
-abort 'Missing a Linux/Unix OS evented API (epoll/kqueue).' unless have_func('kevent') || have_func('epoll_ctl')
-abort 'Missing support for atomic operations (support for C11) - is your compiler updated?' unless have_header('stdatomic.h')
-# abort "Missing OpenSSL." unless have_library("ssl")
-
 $CFLAGS = '-std=c11 -O3 -Wall'
 
 if find_executable('clang')
@@ -19,5 +15,9 @@ end
 
 $CC = ENV['CC'] if ENV['CC']
 $CC = ENV['CPP'] if ENV['CPP']
+
+abort 'Missing a Linux/Unix OS evented API (epoll/kqueue).' unless have_func('kevent') || have_func('epoll_ctl')
+abort 'Missing support for atomic operations (support for C11) - is your compiler updated?' unless have_header('stdatomic.h')
+# abort "Missing OpenSSL." unless have_library("ssl")
 
 create_makefile 'iodine/iodine'
