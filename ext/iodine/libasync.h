bca738b5be65456c6dcc9de3359683ce2f3e37e4
Boaz Segev <Boaz@2be.co.il>
2016-08-13 04:50:42 -0400

    no test

--- ext/iodine/libasync.h
+++ ext/iodine/libasync.h
@@ -14,7 +14,7 @@ Feel free to copy, use and enjoy according to the license provided.
 #include <stdio.h>
 
 #ifndef ASYNC_TEST_INC
-#define ASYNC_TEST_INC 1
+#define ASYNC_TEST_INC 0
 // prints out testing and benchmarking data
 void async_test_library_speed(void);
 #endif
