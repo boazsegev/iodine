f8c23d01b5c5a82c06e0a2131836c56b1e2d0d2b
Bo <bo@bowild.com>
2019-01-16 16:25:57 -0500

    openssl version testing...

--- ext/iodine/extconf.rb
+++ ext/iodine/extconf.rb
@@ -38,9 +38,9 @@ unless ENV['NO_SSL']
   else
     if have_library('crypto') && have_library('ssl')
       puts "Detected OpenSSL library, testing for version."
-      if try_compile("\#include <openssl/ssl.h>\r\#if OPENSSL_VERSION_AT_LEAST(1, 0)\r\#error \"OpenSSL version too small\"\r\#endif\rint main(void) { SSL_library_init(); }")
+      if ((OpenSSL::OPENSSL_VERSION_NUMBER >> 24) >= 16) # try_compile("\#include <openssl/ssl.h>\r\#if !OPENSSL_VERSION_AT_LEAST(1, 0)\r\#error \"OpenSSL version too small\"\r\#endif\rint main(void) { SSL_library_init(); }")
         $defs << "-DHAVE_OPENSSL"
-        puts "Confirmed OpenSSL to be version 1.0.0 or above, compiling with HAVE_OPENSSL."
+        puts "Confirmed OpenSSL to be version 1.0.0 or above (#{OpenSSL::OPENSSL_LIBRARY_VERSION})...\n* Compiling with HAVE_OPENSSL."
       else
         puts "FAILED: OpenSSL version not supported."
       end
