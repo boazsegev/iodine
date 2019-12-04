47809c1d1879962714f548495e35a2036fc22ec6
Ian Ker-Seymer <i.kerseymer@gmail.com>
2019-12-04 10:09:27 -0500

    Clean up specs

--- spec/integration/last_modified_header_spec.rb
+++ spec/integration/last_modified_header_spec.rb
@@ -1,12 +1,6 @@
 require 'http'
 
-RSpec.describe 'Last-Modified Header' do
-  include Spec::Support::IodineServer
-
-  around(:example) do |ex|
-    with_app(:last_modified) { ex.run }
-  end
-
+RSpec.describe 'Last-Modified Header', with_app: :last_modified do
   it 'is parseable by Time#httpdate' do
     response = http_get("/")
     last_modified_str = response.headers['Last-Modified']
