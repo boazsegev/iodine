8476416da07a0f83bae69afc0a23ad199b770d1d
Bo <bo@bowild.com>
2020-02-12 22:54:04 +0000

    docs

--- lib/iodine/connection.rb
+++ lib/iodine/connection.rb
@@ -17,33 +17,40 @@ module Iodine
   # Instances of this class are passed to the callback objects. i.e.:
   #
   #     module MyConnectionCallbacks
+  #
   #       # called when the callback object is linked with a new client
   #       def on_open client
   #          client.is_a?(Iodine::Connection) # => true
   #       end
+  #
   #       # called when data is available
   #       def on_message client, data
   #          client.is_a?(Iodine::Connection) # => true
   #       end
+  #
   #       # called when the server is shutting down, before closing the client
   #       # (it's still possible to send messages to the client)
   #       def on_shutdown client
   #          client.is_a?(Iodine::Connection) # => true
   #       end
+  #
   #       # called when the client is closed (no longer available)
   #       def on_close client
   #          client.is_a?(Iodine::Connection) # => true
   #       end
+  #
   #       # called when all the previous calls to `client.write` have completed
   #       # (the local buffer was drained and is now empty)
   #       def on_drained client
   #          client.is_a?(Iodine::Connection) # => true
   #       end
+  #
   #       # called when timeout was reached, llowing a `ping` to be sent
   #       def ping client
   #          client.is_a?(Iodine::Connection) # => true
   #          clint.close() # close connection on timeout is the default
   #       end
+  #
   #       # Allows the module to be used as a static callback object (avoiding object allocation)
   #       extend self
   #     end
