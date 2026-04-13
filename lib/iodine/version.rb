00ba82bc0070b3776f78a721bc66c963062dd4af
Bo <bo@bowild.com>
2026-04-13 20:36:58 +0700

    fix extension version map (numbers instead of strings)

--- lib/iodine/version.rb
+++ lib/iodine/version.rb
@@ -5,14 +5,15 @@ module Iodine
   VERSION = "0.8.0.rc.02"
   # NeoRack supported extensions.
   def self.extensions
-    return (@extensions ||= { neo_rack: "0.0.2".split(?.),
-                              cookies: "0.0.1".split(?.),
-                              from: "0.0.1".split(?.),
-                              pubsub: "0.0.1".split(?.),
-                              rack: "1.3.0".split(?.),
-                              rest: "0.0.1".split(?.),
-                              sse: "0.0.1".split(?.),
-                              ws: "0.0.1".split(?.)
+    return (@extensions ||= { iodine:   (VERSION.split(?.).map() {|i| i.to_i.to_s == i ? i.to_i : i }),
+                              neo_rack: "0.0.2".split(?.).map(&:to_i),
+                              cookies:  "0.0.1".split(?.).map(&:to_i),
+                              from:     "0.0.1".split(?.).map(&:to_i),
+                              pubsub:   "0.0.1".split(?.).map(&:to_i),
+                              rack:     "1.3.0".split(?.).map(&:to_i),
+                              rest:     "0.0.1".split(?.).map(&:to_i),
+                              sse:      "0.0.1".split(?.).map(&:to_i),
+                              ws:       "0.0.1".split(?.).map(&:to_i)
                             })
   end
 end
