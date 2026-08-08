87a40560c84ef962b5618ea1a76b7a89ce984fb5
Bo <bo@bowild.com>
2026-08-09 01:20:04 +0700

    Restore normal process exit cleanup

--- ext/iodine/iodine.c
+++ ext/iodine/iodine.c
@@ -78,6 +78,7 @@ FIO_SFUNC void *iodine___perform_exit_outside_gvl(void *ignr_) {
 
 FIO_SFUNC void iodine___perform_exit(VALUE ignr_) {
   (void)ignr_;
+  // rb_gc();
   /* iodine runs outside of GVL, but at_exit runs in GVL.. so... */
   rb_thread_call_without_gvl(iodine___perform_exit_outside_gvl,
                              NULL,
